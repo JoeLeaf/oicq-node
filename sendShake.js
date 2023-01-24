6369eb3ea587449b3ff03a0bdc2dc6a51b32bc99
JoeLeaf <42119136+JoeLeaf@users.noreply.github.com>
2023-01-24 19:48:57 +0800

    发送群戳一戳(另类抖动)

--- sendShake.js
+++ sendShake.js
@@ -1,62 +1,61 @@
 const { core } = require("oicq")
 const crypto = require('crypto');
 
-//2023-01-24 
-//发送第30行可以设置动画大小,最大就是3
-async function sendShake(source_group_id, id) {
-    let body = {
-        "1": {
-            "2": {
-                "1": source_group_id
-            }
-        },
-        "2": {
-            "1": 0,
-            "2": 0,
-            "3": 0
-        },
-        "3": {
-            "1": {
-                "2": [
-                    {
-                        "53": {
-                            "1": 2,
-                            "2": {
-                                "1": id,
-                                "3": 0,
-                                "4": crypto.randomBytes(4).readUInt16BE(),
-                                "5": "",
-                                "6": "",
-                                "7": 3,
-                                "10": 0
-                            },
-                            "3": id
-                        }
-                    },
-                    {
-                        "17": {
-                            "1": 0
-                        }
-                    },
-                    {
-                        "37": {
-                            "17": 21908,
-                            "19": {
-                                "15": 65536,
-                                "31": 0,
-                                "41": 0
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-        "4": crypto.randomBytes(2).readUInt16BE(),
-        "5": crypto.randomBytes(4).readUInt16BE(),
-        "8": 0
-    }
-    body = core.pb.encode(body)
-    await bot.sendUni("MessageSvc.PbSendMsg", body);
+//type 1为私聊 0为群聊
+//size是大小最大是3
+
+async function sendShake(recipient, id, size = 3, type = 0) {
+	let body = {
+		"1": {},
+		"2": {
+			"1": 0,
+			"2": 0,
+			"3": 0
+		},
+		"3": {
+			"1": {
+				"2": [
+					{
+						"53": {
+							"1": 2,
+							"2": {
+								"1": id,
+								"3": 0,
+								"4": crypto.randomBytes(4).readUInt16BE(),
+								"5": "",
+								"6": "",
+								"7": parseInt(size),
+								"10": 0
+							},
+							"3": id
+						}
+					},
+					{
+						"17": {
+							"1": 0
+						}
+					},
+					{
+						"37": {
+							"17": 21908,
+							"19": {
+								"15": 65536,
+								"31": 0,
+								"41": 0
+							}
+						}
+					}
+				]
+			}
+		},
+		"4": crypto.randomBytes(2).readUInt16BE(),
+		"5": crypto.randomBytes(4).readUInt16BE(),
+		"8": 0
+	}
+	type ? body[1][1] = { "1": recipient } : body[1][2] = { "1": recipient }
+	body = core.pb.encode(body)
+	await bot.sendUni("MessageSvc.PbSendMsg", body);
 }
+
 //啊发现oicq有可以这样发[{"type":"shake","id":1,"text":"戳一戳"}]
 
