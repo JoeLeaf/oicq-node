4e611d4f30ed853a6b263b0e56409b768b1bf563
JoeLeaf <42119136+JoeLeaf@users.noreply.github.com>
2023-01-24 19:54:14 +0800

    发送大表情

--- sendBigEmoticons.js
+++ sendBigEmoticons.js
@@ -1,65 +1,63 @@
 const { core } = require("oicq")
 const crypto = require('crypto');
-//25行应该传递表情参数,但是我没有去列数据,而且固定影响
-//第30,38,40行,可以传递表情的名字,也可以和我一样
-async function sendBigEmoticons(source_group_id, id) {
-    let body = {
-        1: {
-            2: {
-                1: source_group_id
-            }
-        },
-        2: {
-            1: 0,
-            2: 0,
-            3: 0
-        },
-        3: {
-            1: {
-                2: [
-                    {
-                        53: {
-                            1: 37,
-                            2: {
-                                1: "1",
-                                2: "19",
-                                3: id,
-                                4: 1,
-                                5: 1,
-                                6: "",
-                                7: "/小叶子",
-                                8: "",
-                                9: 1
-                            },
-                            3: 1
-                        }
-                    }, {
-                        1: {
-                            1: "/小叶子",
-                            12: {
-                                1: "[小叶子]咳咳你需要升级一下QQ咯"
-                            }
-                        }
-                    }, {
-                        37: {
-                            17: 21908,
-                            19: {
-                                15: 65536,
-                                31: 0,
-                                41: 0
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-        4: crypto.randomBytes(2).readUInt16BE(),
-        5: crypto.randomBytes(4).readUInt32BE(),
-        8: 1
-    }
-    body = core.pb.encode(body)
-    await bot.sendUni("MessageSvc.PbSendMsg", body);
+
+
+//type 1为私聊 0为群聊
+//看到xyz或者小叶子 表示都可以传值,但是我没有数据,就直接固定,影响不大
+async function sendBigEmoticons(recipient, id, type = 0) {
+	let body = {
+		1: {},
+		2: {
+			1: 0,
+			2: 0,
+			3: 0
+		},
+		3: {
+			1: {
+				2: [
+					{
+						53: {
+							1: 37,
+							2: {
+								1: "1",
+								2: "19",
+								3: id,
+								4: 1,
+								5: 1,
+								6: "",
+								7: "/小叶子",
+								8: "",
+								9: 1
+							},
+							3: 1
+						}
+					},
+					{
+						1: {
+							1: "/小叶子",
+							12: {
+								1: "[小叶子]咳咳你需要升级一下QQ咯"
+							}
+						}
+					},
+					{
+						37: {
+							17: 21908,
+							19: {
+								15: 65536,
+								31: 0,
+								41: 0
+							}
+						}
+					}
+				]
+			}
+		},
+		4: crypto.randomBytes(2).readUInt16BE(),
+		5: crypto.randomBytes(4).readUInt32BE(),
+		8: 1
+	}
+	type ? body[1][1] = { "1": recipient } : body[1][2] = { "1": recipient }
+	body = core.pb.encode(body)
+	await bot.sendUni("MessageSvc.PbSendMsg", body);
 }
-
-
-
