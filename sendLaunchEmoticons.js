a16d57af5f6ccb1640636dde06815d12b495f1b1
JoeLeaf <42119136+JoeLeaf@users.noreply.github.com>
2023-01-24 19:42:25 +0800

    弹射表情,满屏幕那个,私聊好像才有动画

--- sendLaunchEmoticons.js
+++ sendLaunchEmoticons.js
@@ -6,63 +6,61 @@ const crypto = require('crypto');
 //[表情弹射]请使用最新版本手机QQ体验新功能
 //就是这玩意,会满屏幕表情那玩意
 
+//type 1为私聊 0为群聊
 
-async function sendLaunchEmoticons(QQ, id, num) {
-    let body = {
-        "1": {
-            "1": {
-                "1": QQ
-            }
-        },
-        "2": {
-            "1": 0,
-            "2": 0,
-            "3": 0
-        },
-        "3": {
-            "1": {
-                "2": [
-                    {
-                        "53": {
-                            "1": 23,
-                            "2": {
-                                "1": 13,
-                                "2": num,
-                                "3": "小叶子",
-                                "6": {
-                                    "1": id,
-                                    "2": "小叶子",
-                                    "3": "小叶子"
-                                }
-                            },
-                            "3": 13
-                        }
-                    },
-                    {
-                        "1": {
-                            "1": "[小叶子]x数量",
-                            "12": {
-                                "1": "[小叶子]请使用最新版手机QQ体验新功能。"
-                            }
-                        }
-                    },
-                    {
-                        "37": {
-                            "17": 21908,
-                            "19": {
-                                "15": 65536,
-                                "31": 0,
-                                "41": 0
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-        "4": crypto.randomBytes(2).readUInt16BE(),
-        "5": crypto.randomBytes(4).readUInt16BE(),
-        "8": 0
-    };
-    body = core.pb.encode(body)
-    await bot.sendUni("MessageSvc.PbSendMsg", body);
+async function sendLaunchEmoticons(recipient, id, num, type = 0) {
+	let body = {
+		"1": {},
+		"2": {
+			"1": 0,
+			"2": 0,
+			"3": 0
+		},
+		"3": {
+			"1": {
+				"2": [
+					{
+						"53": {
+							"1": 23,
+							"2": {
+								"1": 13,
+								"2": num,
+								"3": "小叶子",
+								"6": {
+									"1": id,
+									"2": "小叶子",
+									"3": "小叶子"
+								}
+							},
+							"3": 13
+						}
+					},
+					{
+						"1": {
+							"1": "[小叶子]x数量",
+							"12": {
+								"1": "[小叶子]请使用最新版手机QQ体验新功能。"
+							}
+						}
+					},
+					{
+						"37": {
+							"17": 21908,
+							"19": {
+								"15": 65536,
+								"31": 0,
+								"41": 0
+							}
+						}
+					}
+				]
+			}
+		},
+		"4": crypto.randomBytes(2).readUInt16BE(),
+		"5": crypto.randomBytes(4).readUInt16BE(),
+		"8": 0
+	};
+	type ? body[1][1] = { "1": recipient } : body[1][2] = { "1": recipient }
+	body = core.pb.encode(body)
+	await bot.sendUni("MessageSvc.PbSendMsg", body);
 }
