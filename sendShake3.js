e9291a9d9c09bf38927cf629d5e154ce84f49f74
JoeLeaf <42119136+JoeLeaf@users.noreply.github.com>
2023-01-24 19:45:08 +0800

    在群里发私聊戳戳的表情和前面2个也不一样

--- sendShake3.js
+++ sendShake3.js
@@ -1,58 +1,57 @@
 const { core } = require("oicq")
 const crypto = require('crypto');
-//22,26,33,35可以自己传递值进去,我没有表情的表,我直接固定,懒就完了
 
-async function sendShake3(source_group_id, id, num) {
-    let body = {
-        "1": {
-            "2": {
-                "1": source_group_id
-            }
-        },
-        "2": {
-            "1": 0,
-            "2": 0,
-            "3": 0
-        },
-        "3": {
-            "1": {
-                "2": [
-                    {
-                        "53": {
-                            "1": 23,
-                            "2": {
-                                "1": id,
-                                "2": num,
-                                "3": "xyz"
-                            },
-                            "3": id
-                        }
-                    },
-                    {
-                        "1": {
-                            "1": "[xyz]",
-                            "12": {
-                                "1": "[略略略]小叶子提示你,你需要手机最新版看这个消息!"
-                            }
-                        }
-                    },
-                    {
-                        "37": {
-                            "17": 0,
-                            "19": {
-                                "15": 0,
-                                "31": 0,
-                                "41": 0
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-        "4": crypto.randomBytes(2).readUInt16BE(),
-        "5": crypto.randomBytes(4).readUInt16BE(),
-        "8": 0
-    }
-    body = core.pb.encode(body)
-    await bot.sendUni("MessageSvc.PbSendMsg", body);
+//type 1为私聊 0为群聊
+//看到xyz或者小叶子 表示都可以传值,但是我没有数据,就直接固定,影响不大
+
+async function sendShake3(recipient, id, num, type = 0) {
+	let body = {
+		"1": {},
+		"2": {
+			"1": 0,
+			"2": 0,
+			"3": 0
+		},
+		"3": {
+			"1": {
+				"2": [
+					{
+						"53": {
+							"1": 23,
+							"2": {
+								"1": id,
+								"2": num,
+								"3": "xyz"
+							},
+							"3": id
+						}
+					},
+					{
+						"1": {
+							"1": "[xyz]",
+							"12": {
+								"1": "[略略略]小叶子提示你,你需要手机最新版看这个消息!"
+							}
+						}
+					},
+					{
+						"37": {
+							"17": 0,
+							"19": {
+								"15": 0,
+								"31": 0,
+								"41": 0
+							}
+						}
+					}
+				]
+			}
+		},
+		"4": crypto.randomBytes(2).readUInt16BE(),
+		"5": crypto.randomBytes(4).readUInt16BE(),
+		"8": 0
+	}
+	type ? body[1][1] = { "1": recipient } : body[1][2] = { "1": recipient }
+	body = core.pb.encode(body)
+	await bot.sendUni("MessageSvc.PbSendMsg", body);
 }
