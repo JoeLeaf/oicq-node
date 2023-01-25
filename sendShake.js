ef7d177612ad7c07deee30e451acee0662a84a39
JoeLeaf <42119136+JoeLeaf@users.noreply.github.com>
2023-01-25 20:26:31 +0800

    发送群戳一戳(另类抖动)

--- sendShake.js
+++ sendShake.js
@@ -4,6 +4,9 @@ const crypto = require('crypto');
 //type 1为私聊 0为群聊
 //size是大小最大是3
 
+//啊发现oicq有可以这样发[{"type":"shake","id":1,"text":"戳一戳"}]
+//嗯新的发现,大于1999也就是2000就变成超级会员表情了,然后结构发生了变化,oicq发不出来
+//下面免会员可发会员戳戳
 async function sendShake(recipient, id, size = 3, type = 0) {
 	let body = {
 		"1": {},
@@ -19,15 +22,20 @@ async function sendShake(recipient, id, size = 3, type = 0) {
 						"53": {
 							"1": 2,
 							"2": {
-								"1": id,
+								"1": 1,
 								"3": 0,
-								"4": crypto.randomBytes(4).readUInt16BE(),
+								"4": 1,
 								"5": "",
-								"6": "",
+								"6": "7.2.0",
 								"7": parseInt(size),
 								"10": 0
 							},
-							"3": id
+							"3": 1
+						}
+					},
+					{
+						"1": {
+							"1": "[刷RNB系统]请使用最新版手机QQ体验新功能。"
 						}
 					},
 					{
@@ -51,11 +59,20 @@ async function sendShake(recipient, id, size = 3, type = 0) {
 		"4": crypto.randomBytes(2).readUInt16BE(),
 		"5": crypto.randomBytes(4).readUInt16BE(),
 		"8": 0
+	};
+	if (id > 1999) {
+		body[3][1][2][0][53][2][1] = 126
+		body[3][1][2][0][53][2][4] = id
+		body[3][1][2][0][53][3] = 126
+	} else {
+		body[3][1][2][0][53][2][1] = id
+		body[3][1][2][0][53][2][4] = 1
+		body[3][1][2][0][53][3] = id
 	}
 	type ? body[1][1] = { "1": recipient } : body[1][2] = { "1": recipient }
 	body = core.pb.encode(body)
 	await bot.sendUni("MessageSvc.PbSendMsg", body);
 }
 
-//啊发现oicq有可以这样发[{"type":"shake","id":1,"text":"戳一戳"}]
+
 
